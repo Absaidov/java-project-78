609c601ca9e9f3515ea2d6d0e3d5dfce6e713698
Absaidov <dabsaidov@bk.ru>
2023-06-13 13:41:41 +0300

    fixin error build

--- app/src/test/java/hexlet/code/AppTest3.java
+++ app/src/test/java/hexlet/code/AppTest3.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static hexlet.code.App.hello;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class AppTest2 {
+public class AppTest3 {
     @Test
     public void testApp() {
         String expected = "Hello World! LOL";
